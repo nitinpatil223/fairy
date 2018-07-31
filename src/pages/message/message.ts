c0ccf8f70afb3f2845b3f56717ac243d2a8e3848
Sonali Sanap <sonalisanap@benchmarkitsolutions.com>
2018-07-31 18:27:59 +0530

    next pages

--- src/pages/message/message.ts
+++ src/pages/message/message.ts
@@ -15,7 +15,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MessagePage {
 
+  hotels;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.hotels = "all";
   }
 
   ionViewDidLoad() {
