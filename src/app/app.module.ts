7ab49f428ee7708ac6e14abd77347d0b6853165e
Nitin Patil <nitinpatil@benchmarkitsolutions.com>
2018-08-13 17:27:46 +0530

    book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    //  MyApp,
+     MyApp,
     // WelcomePage,
     // LoginPage,
     // SignupPage,
--- src/pages/book-table/book-table.ts
+++ src/pages/book-table/book-table.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { HelloIonicPage } from '../hello-ionic/hello-ionic';
 
@@ -15,13 +15,9 @@ import { HelloIonicPage } from '../hello-ionic/hello-ionic';
   templateUrl: 'book-table.html',
 })
 export class BookTablePage {
-  @ViewChild('pageRef') pageRef: ElementRef;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public renderer: Renderer, public viewCtrl: ViewController, private el: ElementRef) {
-    // this.renderer.setElementClass(viewCtrl.pageRef().nativeElement, 'waiting-popup', true);
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-
-  
   welcome() {
     this.navCtrl.push(HelloIonicPage, {}, { animate: true, animation: 'transition', duration: 1000, direction: 'forward' });
   }
